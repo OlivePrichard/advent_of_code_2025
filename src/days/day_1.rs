706c694afeeaace8c7a010e287cd6dd3d9c89cd9
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2025-12-12 03:18:58 -0500

    day 1

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -5,9 +5,42 @@ fn read_file() -> String {
 }
 
 pub fn first() {
-
+    let code = read_file().lines().map(|s| {
+        let n: i32 = s[1..].parse().unwrap();
+        if s[0..1] == *"L" {
+            -n
+        } else {
+            n
+        }
+    }).fold((50, 0), |acc, turn| {
+        let mut pos = acc.0 + turn;
+        pos = pos.rem_euclid(100);
+        if pos == 0 {
+            (pos, acc.1 + 1)
+        } else {
+            (pos, acc.1)
+        }
+    }).1;
+    println!("{}", code);
 }
 
 pub fn second() {
-
+    let code = read_file().lines().map(|s| {
+        let n: i32 = s[1..].parse().unwrap();
+        if s[0..1] == *"L" {
+            -n
+        } else {
+            n
+        }
+    }).fold((50, 0), |(prev_pos, zero_count), turn| {
+        // println!("{} {} {}", turn, prev_pos, zero_count);
+        let pos = prev_pos + turn;
+        if turn > 0 {
+            (pos % 100, zero_count + pos / 100)
+        } else {
+            let wrap = (100 - prev_pos) % 100 - turn;
+            (pos.rem_euclid(100), zero_count + wrap / 100)
+        }
+    }).1;
+    println!("{}", code);
 }

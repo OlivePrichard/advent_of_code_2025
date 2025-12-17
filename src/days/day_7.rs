d210ad3331372219f3787f7290942182a2a2593a
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2025-12-17 09:29:27 -0500

    day 7

--- src/days/day_7.rs
+++ src/days/day_7.rs
@@ -1,13 +1,50 @@
-use std::fs;
+use std::{collections::{HashMap, hash_map::Entry}, fs};
 
 fn read_file() -> String {
     fs::read_to_string("src/inputs/day_7.txt").expect("Error reading the file")
 }
 
 pub fn first() {
+    let mut lines = read_file().lines().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<_>>();
+    let mut splits = 0;
+    for i in 1..lines.len() {
+        for j in 0..lines[i].len() {
+            if lines[i-1][j] != '|' && lines[i-1][j] != 'S' {
+                continue;
+            }
+            if lines[i][j] == '^' {
+                lines[i][j-1] = '|';
+                lines[i][j+1] = '|';
+                splits += 1;
+            } else {
+                lines[i][j] = '|';
+            }
+        }
+    }
+    println!("{}", splits);
+}
 
+fn count_timelines(i: usize, j: usize, manifold: &Vec<Vec<char>>, memoize: &mut HashMap<(usize, usize), u64>) -> u64 {
+    if i == manifold.len() - 1 {
+        return 1;
+    }
+    let key = (i, j);
+    if let Entry::Occupied(val) = memoize.entry(key) {
+        return *val.get();
+    }
+    let i = i + 1;
+    let count = if manifold[i][j] == '^' {
+        count_timelines(i, j-1, manifold, memoize) + count_timelines(i, j+1, manifold, memoize)
+    } else {
+        count_timelines(i, j, manifold, memoize)
+    };
+    _ = memoize.insert(key, count);
+    count
 }
 
 pub fn second() {
-
+    let lines = read_file().lines().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<_>>();
+    let j = lines[0].iter().position(|&c| c == 'S').unwrap();
+    let timelines = count_timelines(0, j, &lines, &mut HashMap::new());
+    println!("{}", timelines);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 mod days;
 
-use days::day_6::*;
+use days::day_7::*;
 
 fn time(function: fn()) {
     let start = std::time::Instant::now();
